137addcf708cb03fb7d5df958cab356969c932a5
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2022-09-28 14:57:30 +0700

    edit navbar

---
+++ v1.1/includes/navbar.php
@@ -0,0 +1,40 @@
+<nav class="navbar fixed-top navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="./src/logo.png" alt="logo" width="120">
+            </a>
+            <div class="navbar-wrapper">
+                <button class="custom-button btn navbar-btn contact" type="button">Contact Us</button>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars icon"></i>
+                </button>
+            </div>
+    
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto w-100 justify-content-end">
+                    <li class="nav-item active" id='home-nav'>
+                        <a class="nav-link paragraph" href="#">Home</a>
+                    </li>
+                    <li class="nav-item" id='about-nav'>
+                        <a class="nav-link paragraph" href="#about">About</a>
+                    </li>
+                    <li class="nav-item" id='field-nav'>
+                        <a class="nav-link paragraph" href="#fields">Business Fields</a>
+                    </li>
+                    <li class="nav-item" id='product-nav'>
+                        <a class="nav-link paragraph" href="./templates/product.html">Products</a>
+                    </li>
+                    <li class="nav-item" id='why-nav'>
+                        <a class="nav-link paragraph" href="#why">Why Us ?</a>
+                    </li>
+                    <li class="nav-item" id='update-nav'>
+                        <a class="nav-link paragraph" href="#update">Update</a>
+                    </li>
+                    <li class="nav-item" id='team-nav'>
+                        <a class="nav-link paragraph" href="#team">Team</a>
+                    </li>
+                    <button class="custom-button btn navbar-btn contact" type="button">Contact Us</button>
+                </ul>
+            </div>
+        </div>
+</nav>
--- v1.1/index.php
+++ v1.1/index.php
@@ -1,5 +1,5 @@
 <?php
-include "./php/connect.php";
+include "./api/connect.php";
 
 $id = 1;
 $sql = "SELECT * from home WHERE login_id = '$id'";
@@ -85,47 +85,7 @@ if (isset($home) && isset($about) && isset($business_fields_info) && isset($why_
 <body>
     
     <!-- Navbar -->
-    <nav class="navbar fixed-top navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="./src/logo.png" alt="logo" width="120">
-            </a>
-            <div class="navbar-wrapper">
-                <button class="custom-button btn navbar-btn contact" type="button">Contact Us</button>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-bars icon"></i>
-                </button>
-            </div>
-    
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto w-100 justify-content-end">
-                    <li class="nav-item active">
-                        <a class="nav-link paragraph" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link paragraph" href="#about">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link paragraph" href="#fields">Business Fields</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link paragraph" href="./templates/product.html">Products</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link paragraph" href="#why">Why Us ?</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link paragraph" href="#update">Update</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link paragraph" href="#team">Team</a>
-                    </li>
-                    <button class="custom-button btn navbar-btn contact" type="button">Contact Us</button>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
+    <?php include 'includes/navbar.php' ?>
     <!-- Home Section Page -->
     <?php
     if (isset($row_home)) {
